0d1e58af641b65ca12a0d48598e261f4c915f441
“Chiefopoku” <kwabenaopokujnr@gmail.com>
2024-07-01 07:09:06 -0700

    es6 basics

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,9 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
- // if (trueOrFalse) {
- //   task = true;
- //   task2 = false;
- // }
+  if (trueOrFalse) {
+    return [task, task2];
+  }
 
   return [task, task2];
 }
